49dd69a8c19f7e563fdf37e6415e09aadf250d52
salmane <salmajo2001@gmail.com>
2026-05-30 02:34:32 +0100

    fix: move interrupt handling to internal use of SocketChannel

--- src/main/java/audioresource/source/TCPSource.java
+++ src/main/java/audioresource/source/TCPSource.java
@@ -2,37 +2,44 @@ package audioresource.source;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 
 public class TCPSource implements Source{
     private Socket socket;
     private InputStream inputStream;
+    private SocketChannel channel;
+    private String host;
+    private int port;
     private long position = 0;
     private int bytesRead = 0;
 
 
-    public TCPSource (String host, int port) throws IOException {
-        socket = new Socket(host, port);
-        inputStream = socket.getInputStream();
+    public TCPSource (String host, int port) {
+        try {
+            channel = SocketChannel.open(new InetSocketAddress(host, port));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
     @Override
     public boolean isSeekable(){return false;}
     @Override
     public void open() {
-        System.out.println("You do not need to open a socket");
+
+        try {
+            socket = new Socket(host, port);
+            inputStream = socket.getInputStream();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
     @Override
     public int read(ByteBuffer buffer) {
         try {
-            int remaining = buffer.remaining();
-            if (remaining == 0) return 0;
-            bytesRead = inputStream.read(buffer.array(), buffer.position(), remaining);
-            if (bytesRead > 0) {
-                buffer.position(buffer.position() + bytesRead);
-                position += bytesRead;
-            }
-            return bytesRead;
+            return channel.read(buffer);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
